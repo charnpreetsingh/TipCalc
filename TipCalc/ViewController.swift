f326de1f7b3e9408ca44d0c79e6ebc260201f2be
charnpreetsingh <csingh02@calpoly.edu>
2017-05-10 16:43:57 -0700

    initial commit

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var billAmount: UITextField!
+    @IBOutlet var tipLabel: UILabel!
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var tipIdx: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapGestureRecognizer(_ sender: Any) {
+        view.endEditing(true)
+    }
 
+    @IBAction func updateTip(_ sender: Any) {
+        let tipVals = [0.10, 0.15, 0.20]
+        
+        let bill = Double(billAmount.text!) ?? 0
+        let tip = bill * tipVals[tipIdx.selectedSegmentIndex]
+        tipLabel.text = String(format:"$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", bill + tip)
+    }
+    
+    @IBAction func updateAgain(_ sender: Any) {
+        let tipVals = [0.10, 0.15, 0.20]
+        
+        let bill = Double(billAmount.text!) ?? 0
+        let tip = bill * tipVals[tipIdx.selectedSegmentIndex]
+        tipLabel.text = String(format:"$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", bill + tip)
+    }
 }
 
